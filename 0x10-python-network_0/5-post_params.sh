c82af39efe63cbfeb98b85c8b486ac1d66a40dd7
roveld99 <roveld99@gmail.com>
2022-03-17 20:10:04 +0300

    python-network

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -sd "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
+curl -s "$1" -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
